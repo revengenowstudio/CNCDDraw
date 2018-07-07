c1c3ac85ee74caf2ebb88b0b05c649a399738ede
FunkyFr3sh <cc.red.alert.1@googlemail.com>
2018-07-07 09:17:26 +0200

    Revert "minimize window in RestoreDisplayMode"
    This reverts commit e4254f7f220c39fda17b025341df211e32f1c6b8.

--- src/main.c
+++ src/main.c
@@ -231,9 +231,7 @@ HRESULT __stdcall ddraw_RestoreDisplayMode(IDirectDrawImpl *This)
         WaitForSingleObject(This->render.thread, INFINITE);
         This->render.thread = NULL;
     }
-
-    ShowWindow(This->hWnd, SW_MINIMIZE);
-
+    
     if(!ddraw->windowed)
     {
         ChangeDisplaySettings(&This->mode, 0);
