f4375f60c43832745cfcc827a306184ae841f753
FunkyFr3sh <cc.red.alert.1@googlemail.com>
2019-08-07 13:26:40 +0200

    sync with war2 branch

--- src/main.c
+++ src/main.c
@@ -1189,6 +1189,28 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 
             break;
 
+        //workaround for a bug where sometimes a background window steals the focus
+        case WM_WINDOWPOSCHANGING:
+        {
+            if (ddraw->locked)
+            {
+                WINDOWPOS *pos = (WINDOWPOS *)lParam;
+                
+                if (pos->flags == SWP_NOMOVE + SWP_NOSIZE)
+                {
+                    mouse_unlock();
+                    
+                    if (GetForegroundWindow() == ddraw->hWnd)
+                        mouse_lock();
+                }
+            }
+            break;
+        }
+
+        case WM_MOUSELEAVE:
+            mouse_unlock();
+            return 0;
+
         case WM_ACTIVATE:
             if (wParam == WA_ACTIVE || wParam == WA_CLICKACTIVE)
             {
@@ -1228,28 +1250,6 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
                     }
                 }
             }
-            return 0;
-
-        //workaround for a bug where sometimes a background window steals the focus
-        case WM_WINDOWPOSCHANGING:
-        {
-            if (ddraw->locked)
-            {
-                WINDOWPOS *pos = (WINDOWPOS *)lParam;
-                
-                if (pos->flags == SWP_NOMOVE + SWP_NOSIZE)
-                {
-                    mouse_unlock();
-                    
-                    if (GetForegroundWindow() == ddraw->hWnd)
-                        mouse_lock();
-                }
-            }
-            break;
-        }
-
-        case WM_MOUSELEAVE:
-            mouse_unlock();
             return 0;
 
         case WM_ACTIVATEAPP:
