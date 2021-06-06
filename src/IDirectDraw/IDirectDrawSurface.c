76634ab84ed543333bbdbb7460096feeeb8348c1
FunkyFr3sh <cc.red.alert.1@googlemail.com>
2021-06-06 02:28:30 +0200

    adjust logging

--- src/IDirectDraw/IDirectDrawSurface.c
+++ src/IDirectDraw/IDirectDrawSurface.c
@@ -240,9 +240,9 @@ HRESULT __stdcall IDirectDrawSurface__GetDC(IDirectDrawSurfaceImpl *This, HDC FA
 
 HRESULT __stdcall IDirectDrawSurface__GetFlipStatus(IDirectDrawSurfaceImpl *This, DWORD a)
 {
-    dprintfex("NOT_IMPLEMENTED -> %s(This=%p)\n", __FUNCTION__, This);
+    dprintfex("-> %s(This=%p)\n", __FUNCTION__, This);
     HRESULT ret = DD_OK;
-    dprintfex("NOT_IMPLEMENTED <- %s\n", __FUNCTION__);
+    dprintfex("<- %s\n", __FUNCTION__);
     return ret;
 }
 
